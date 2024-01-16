cfb98962acf0be99f29a71b0f7462260e464a540
et120 <e.trotter120@gmail.com>
2024-01-16 12:41:21 -0800

    initial repo - font, file struct, icon

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+// Title Page
+let title = document.getElementById("title");
+let area = document.getElementById("area");
+let startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener('click', );
